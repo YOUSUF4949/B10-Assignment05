df7ac5032cd19e9fe3f1fd4f0f16c8f4fe6bd4d9
YOUSUF4949 <yousufalibangladesh49@gmail.com>
2024-09-25 09:25:30 -0700

    added javascript functionality in history page

--- donate-culaction.js
+++ donate-culaction.js
@@ -37,13 +37,46 @@ donateBtn.addEventListener('click', function(event){
     let mainBalance = document.getElementById('main-balance').innerText;
     let donateCurrentBalance = document.getElementById('current-balance').innerText;
     let inputBalance = document.getElementById('donate-amount').value;
-    // Decriment money of the main balance for the donate;
-    let donate = parseInt(mainBalance) - parseInt(inputBalance);
-    document.getElementById('main-balance').innerText = donate;
-    // add the money in donate current balance;
-    let addCurrentBalance = parseInt(donateCurrentBalance) + parseInt(inputBalance);
-    document.getElementById('current-balance').innerText = addCurrentBalance;
+    if((parseInt(mainBalance) > parseInt(inputBalance) && parseInt(inputBalance) > 0)){ 
+        let updateMainBalance = parseInt(mainBalance) - parseInt(inputBalance);
+        document.getElementById('main-balance').innerText = updateMainBalance;
+        // add the current balance
+        let donateAddition = parseInt(donateCurrentBalance) + parseInt(inputBalance);
+        document.getElementById('current-balance').innerText = donateAddition;
 
-    // clear input field;
-    document.getElementById('donate-amount').value = '';
+        // History empty page hidden;
+        let emptyPage = document.getElementById('no-culaction');
+            emptyPage.classList.add('hidden');
+        // Culaction Data added in history page;
+        let parent = document.getElementById('culection-data-container')
+        let dataBox = document.createElement('div');
+        let donateTitle01 = document.getElementById('donateTitle01').innerText;
+
+        parent.appendChild(dataBox);
+        // create donate colaction title and text;
+        let createDonateTitle = document.createElement('h2');
+        let createText = document.createTextNode(`${inputBalance}, ${donateTitle01}`);
+        createDonateTitle.appendChild(createText);
+        dataBox.appendChild(createDonateTitle);
+        console.log(createDonateTitle)
+        // create date donation;
+        let date = new Date();
+        let donateTime = document.createElement('p');
+        let createTimeText = document.createTextNode(` ${date}`);
+        donateTime.appendChild(createTimeText);
+        console.log(donateTime)
+        // add the create all elements in parent(data) Tag
+        dataBox.appendChild(createDonateTitle);
+        dataBox.appendChild(donateTime);
+        // styling the create element;
+        dataBox.classList.add('border', 'py-4', 'px-3', 'lg:px-10');
+        createDonateTitle.classList.add('font-bold','text-xl', 'lg:text-2xl', 'pb-5');
+        donateTime.classList.add('font-semibold')
+        // clear input field after the submit balance;
+        document.getElementById('donate-amount').value = '';
+    }else{
+        alert('Invalid Amount');
+    }
 })
+
+
